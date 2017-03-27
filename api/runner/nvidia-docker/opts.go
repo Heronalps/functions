b82ada434b0ffc09f3c242ba0eedeb35679e40be
cmdhema <cmdhema@gmail.com>
2017-03-27 22:53:24 +0900

    Add nvidia-docker api

--- api/runner/nvidia-docker/opts.go
+++ api/runner/nvidia-docker/opts.go
@@ -1,87 +1,122 @@
 // Copyright (c) 2015-2016, NVIDIA CORPORATION. All rights reserved.
 
-package main
+package nvidia_docker
 
 import (
-	"fmt"
+	//"fmt"
 
-	"github.com/NVIDIA/nvidia-docker/src/docker"
-	"github.com/NVIDIA/nvidia-docker/src/nvidia"
+	//"github.com/NVIDIA/nvidia-docker/src/docker"
+	//"github.com/NVIDIA/nvidia-docker/src/nvidia"
 )
 
-func GenerateLocalArgs(image string, vols []string) ([]string, error) {
-	cv, err := nvidia.GetCUDAVersion()
-	if err != nil {
-		return nil, err
-	}
-	if err := cudaSupported(image, cv); err != nil {
-		return nil, err
-	}
+//func GenerateLocalArgs(image string, vols []string) ([]string, error) {
+//	cv, err := nvidia.GetCUDAVersion()
+//	if err != nil {
+//		return nil, err
+//	}
+//	if err := cudaSupported(image, cv); err != nil {
+//		return nil, err
+//	}
+//
+//	d, err := devicesArgs()
+//	if err != nil {
+//		return nil, err
+//	}
+//	v, err := volumesArgs(vols)
+//	if err != nil {
+//		return nil, err
+//	}
+//	return append(d, v...), nil
+//}
 
-	d, err := devicesArgs()
-	if err != nil {
-		return nil, err
-	}
-	v, err := volumesArgs(vols)
-	if err != nil {
-		return nil, err
-	}
-	return append(d, v...), nil
-}
-
-func devicesArgs() ([]string, error) {
-	var args []string
-
-	cdevs, err := nvidia.GetControlDevicePaths()
-	if err != nil {
-		return nil, err
-	}
-	for i := range cdevs {
-		args = append(args, fmt.Sprintf("--device=%s", cdevs[i]))
-	}
-
-	devs, err := nvidia.LookupDevices(nvidia.LookupMinimal)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(GPU) == 0 {
-		for i := range devs {
-			args = append(args, fmt.Sprintf("--device=%s", devs[i].Path))
-		}
-	} else {
-		devs, err := nvidia.FilterDevices(devs, GPU)
-		if err != nil {
-			return nil, err
-		}
-		for i := range devs {
-			args = append(args, fmt.Sprintf("--device=%s", devs[i].Path))
-		}
-	}
-	return args, nil
-}
-
-func volumesArgs(vols []string) ([]string, error) {
-	args := make([]string, 0, len(vols))
-
-	drv, err := nvidia.GetDriverVersion()
-	if err != nil {
-		return nil, err
-	}
-	for _, vol := range nvidia.Volumes {
-		for _, v := range vols {
-			if v == vol.Name {
-				// Check if the volume exists locally otherwise fallback to using the plugin
-				n := fmt.Sprintf("%s_%s", vol.Name, drv)
-				if _, err := docker.VolumeInspect(n); err == nil {
-					args = append(args, fmt.Sprintf("--volume=%s:%s:%s", n, vol.Mountpoint, vol.MountOptions))
-				} else {
-					args = append(args, fmt.Sprintf("--volume-driver=%s", nvidia.DockerPlugin))
-					args = append(args, fmt.Sprintf("--volume=%s:%s:%s", n, vol.Mountpoint, vol.MountOptions))
-				}
-				break
-			}
-		}
-	}
-	return args, nil
-}
+//func DevicesArgs() ([]string, error) {
+//	var args []string
+//
+//	cdevs, err := nvidia.GetControlDevicePaths()
+//	if err != nil {
+//		return nil, err
+//	}
+//	for i := range cdevs {
+//		args = append(args, fmt.Sprintf("--device=%s", cdevs[i]))
+//	}
+//
+//	devs, err := nvidia.LookupDevices(nvidia.LookupMinimal)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	if len(GPU) == 0 {
+//		for i := range devs {
+//			args = append(args, fmt.Sprintf("--device=%s", devs[i].Path))
+//		}
+//	} else {
+//		devs, err := nvidia.FilterDevices(devs, GPU)
+//		if err != nil {
+//			return nil, err
+//		}
+//		for i := range devs {
+//			args = append(args, fmt.Sprintf("--device=%s", devs[i].Path))
+//		}
+//	}
+//	return args, nil
+//}
+//
+//func VolumesArgs(vols []string) ([]string, error) {
+//	args := make([]string, 0, len(vols))
+//
+//	drv, err := nvidia.GetDriverVersion()
+//	if err != nil {
+//		return nil, err
+//	}
+//	for _, vol := range nvidia.Volumes {
+//		for _, v := range vols {
+//			if v == vol.Name {
+//				// Check if the volume exists locally otherwise fallback to using the plugin
+//				n := fmt.Sprintf("%s_%s", vol.Name, drv)
+//				if _, err := docker.VolumeInspect(n); err == nil {
+//					args = append(args, fmt.Sprintf("--volume=%s:%s:%s", n, vol.Mountpoint, vol.MountOptions))
+//				} else {
+//					args = append(args, fmt.Sprintf("--volume-driver=%s", nvidia.DockerPlugin))
+//					args = append(args, fmt.Sprintf("--volume=%s:%s:%s", n, vol.Mountpoint, vol.MountOptions))
+//				}
+//				break
+//			}
+//		}
+//	}
+//	return args, nil
+//}
+//
+//func enableCUDA(image string) (error) {
+//	cv, err := nvidia.GetCUDAVersion()
+//	if err != nil {
+//		return err
+//	}
+//	if err := cudaSupported(image, cv); err != nil {
+//		return err
+//	}
+//
+//	return err
+//}
+//
+//func cudaSupported(image, version string) error {
+//	var vmaj, vmin int
+//	var lmaj, lmin int
+//
+//	label, err := docker.Label(image, labelCUDAVersion)
+//	if err != nil {
+//		return err
+//	}
+//	if label == "" {
+//		return nil
+//	}
+//	if _, err := fmt.Sscanf(version, "%d.%d", &vmaj, &vmin); err != nil {
+//		return err
+//	}
+//	if _, err := fmt.Sscanf(label, "%d.%d", &lmaj, &lmin); err != nil {
+//		return err
+//	}
+//	if lmaj > vmaj || (lmaj == vmaj && lmin > vmin) {
+//		return fmt.Errorf("unsupported CUDA version: driver %s < image %s", version, label)
+//	}
+//	return nil
+//}
--- api/runner/task.go
+++ api/runner/task.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"io"
 	"time"
-
+	//"fmt"
+	"log"
 	"github.com/fsouza/go-dockerclient"
 	"github.com/iron-io/functions/api/runner/task"
 	"github.com/iron-io/runner/drivers"
+	"github.com/NVIDIA/nvidia-docker/src/nvidia"
 )
 
 type containerTask struct {
@@ -15,6 +17,32 @@ type containerTask struct {
 	cfg    *task.Config
 	canRun chan bool
 }
+
+func (t *containerTask) Volumes() [][2]string {
+	//vols, err := nvidia_docker.VolumesNeeded(t.cfg.Image)
+	//if  err != nil {
+	//	volumes := nvidia_docker.VolumesArgs(vols)
+	//	fmt.Println(volumes)
+	//	devices := nvidia_docker.DevicesArgs()
+	//	fmt.Println(devices)
+	//}
+
+	//log.Println("Discovering GPU devices")
+	//Devices, err = nvidia.LookupDevices()
+	//assert(err)
+	//
+	//log.Println("Provisioning volumes at", "/var/lib/nvidia-docker/volumes")
+	//Volumes, err = nvidia.LookupVolumes("/var/lib/nvidia-docker/volumes")
+	//assert(err)
+	//
+	//fmt.Println("Device")
+	//fmt.Println(Devices)
+	//fmt.Println("Volumes")
+	//fmt.Println(Volumes)
+
+	return [][2]string{}
+}
+
 
 func (t *containerTask) Command() string { return "" }
 
@@ -36,7 +64,7 @@ func (t *containerTask) Route() string                      { return "" }
 func (t *containerTask) Image() string                      { return t.cfg.Image }
 func (t *containerTask) Timeout() time.Duration             { return t.cfg.Timeout }
 func (t *containerTask) Logger() (stdout, stderr io.Writer) { return t.cfg.Stdout, t.cfg.Stderr }
-func (t *containerTask) Volumes() [][2]string               { return [][2]string{} }
+//func (t *containerTask) Volumes() [][2]string               { return [][2]string{} }
 func (t *containerTask) WorkDir() string                    { return "" }
 
 func (t *containerTask) Close()                 {}
